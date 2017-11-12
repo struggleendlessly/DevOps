576db10610ffc0efc1fd92ea694c6ccf60cddc51
Oleksandr Dubyna <se8se@hotmail.com>
2017-11-12 17:45:57 +0200

    development

--- DevOps/DevOps/Program.cs
+++ DevOps/DevOps/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace DevOps
     {
         static void Main(string[] args)
         {
-
+            Debug.WriteLine("test");
         }
     }
 }
